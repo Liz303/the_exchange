c8de9b8ccd53505b0b2b8328da8d89c5972fe69d
Mary E Henderson <lhenderson303@gmail.com>
2024-01-11 12:09:28 -0700

    update font

--- Components/Layout/index.js
+++ Components/Layout/index.js
@@ -13,13 +13,6 @@ const Layout = ({ children, hasNews }) => {
    
   }, []);
 
-  const draw = (ctx, frameCount) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = '#ffffff';
-    ctx.beginPath();
-    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI);
-    ctx.fill();
-  }
 
   return (
     <div >
@@ -68,7 +61,7 @@ const Layout = ({ children, hasNews }) => {
       </Head>
       <Header />
       <main className={styles.mainContainer}>
-        <Canvas draw={draw} />
+        <IntroSection/>
         {children}
       </main>
       <Footer />
