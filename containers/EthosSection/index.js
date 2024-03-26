86353b588321bb82bd367c833e053d383c29d32f
Mary E Henderson <lhenderson303@gmail.com>
2024-03-26 13:18:23 -0600

    switch the files for Ethos and Info sections
    switch component behaviors

--- containers/EthosSection/index.js
+++ containers/EthosSection/index.js
@@ -1,47 +1,68 @@
 "use client";
+import { useMemo, useRef } from "react";
 import * as Scrollytelling from "@bsmnt/scrollytelling";
 
 export default function Ethos() {
-  return (
-    <section>
-      <section className="section">
-        <div className="wrapper">
-          <h1>Section1</h1>
-        </div>
-      </section>
+  const containerRef = useRef(null);
 
-      <section className="section orange">
-        <Scrollytelling.Root
-          scrub={false}
-          callbacks={{
-            onEnterBack: (st) => {
-              st.animation.seek(0).play();
-            },
-          }}
+  const sectionData = [
+    { label: "SECTION 1" },
+    { label: "SECTION 2" },
+    { label: "SECTION 3" },
+  ];
+
+  const horizontalScrollTween = useMemo(() => {
+    return { xPercent: -100 * (sectionData.length - 1), ease: "none" };
+  }, [sectionData.length]);
+
+  const sectionElements = sectionData.map((section, index) => (
+    <Scrollytelling.Animation
+      key={index}
+      tween={{
+        start: 0,
+        end: 100,
+        to: horizontalScrollTween,
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h2>{section.label}</h2>
+      </div>
+    </Scrollytelling.Animation>
+  ));
+
+  return (
+    <Scrollytelling.Root defaults={{ ease: "none" }} scrub={1} end="+=3500">
+      <section>
+
+        <Scrollytelling.Animation
+          tween={{ start: 0, end: 1, to: { rotate: 360 } }}
         >
-          <div
-            className="wrapper"
-            style={{
-              height: "100vh",
-            }}
+          <h1>This element will spin.</h1>
+        </Scrollytelling.Animation>
+    
+          <Scrollytelling.Pin
+            childHeight={"100vh"}
+            pinSpacerHeight={"500vh"}
+            pinSpacerClassName={"pin-spacer"}
+            childClassName={"pin-style"}
+            ref={containerRef}
           >
-            <Scrollytelling.Animation
-              tween={{ start: 0, end: 1, to: { rotate: 360 } }}
-            >
-              <h1>This element will spin.</h1>
-            </Scrollytelling.Animation>
-            <Scrollytelling.Animation
-              tween={{
-                start: 0.25,
-                end: 0.75,
-                to: { scale: 1.5, yoyo: true, repeat: -1 },
-              }}
-            >
-              <h1>Yoyo Text</h1>
-            </Scrollytelling.Animation>
-          </div>
-        </Scrollytelling.Root>
+            {sectionElements}
+          </Scrollytelling.Pin>
+
+        <div className="header">
+          <h1>End of horizontal scroll</h1>
+        </div>
+
       </section>
-    </section>
+    </Scrollytelling.Root>
   );
 }
--- containers/InfoSection/index.js
+++ containers/InfoSection/index.js
@@ -1,62 +1,27 @@
 "use client";
-import { useMemo, useRef } from "react";
 import * as Scrollytelling from "@bsmnt/scrollytelling";
 
 export default function Info() {
-  const containerRef = useRef(null);
-
-  const sectionData = [
-    { label: "SECTION 1" },
-    { label: "SECTION 2" },
-    { label: "SECTION 3" },
-  ];
-
-  const horizontalScrollTween = useMemo(() => {
-    return { xPercent: -100 * (sectionData.length - 1), ease: "none" };
-  }, [sectionData.length]);
-
-  const sectionElements = sectionData.map((section, index) => (
-    <Scrollytelling.Animation
-      key={index}
-      tween={{
-        start: 0,
-        end: 100,
-        to: horizontalScrollTween,
-      }}
-    >
-      <div
-        style={{
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+  return (
+    <section className="section orange">
+      <Scrollytelling.Root
+        scrub={false}
+        callbacks={{
+          onEnterBack: (st) => {
+            st.animation.seek(0).play();
+          },
         }}
       >
-        <h2>{section.label}</h2>
-      </div>
-    </Scrollytelling.Animation>
-  ));
-
-  return (
-    <main>
-      <div className="header">
-        <h1>horizontal scroll</h1>
-      </div>
-      <Scrollytelling.Root defaults={{ ease: "none" }} scrub={1} end="+=3500">
-        <Scrollytelling.Pin
-          childHeight={"100vh"}
-          pinSpacerHeight={"500vh"}
-          pinSpacerClassName={"pin-spacer"}
-          childClassName={"pin-style"}
-          ref={containerRef}
+        <div
+          className="wrapper"
+          style={{
+            height: "100vh",
+          }}
         >
-          {sectionElements}
-        </Scrollytelling.Pin>
+        <h1> Placeholder</h1>
+        </div>
       </Scrollytelling.Root>
-      <div className="header">
-        <h1>End of horizontal scroll</h1>
-      </div>
-    </main>
+    </section>
+
   );
 }
