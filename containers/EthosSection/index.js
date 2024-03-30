0b689aeb8f54b6937b9042503b51c4138bbdecb7
IB - Scott <scott@impossible-bureau.com>
2024-03-30 10:43:22 -0600

    initial rescroll

--- containers/EthosSection/index.js
+++ containers/EthosSection/index.js
@@ -1,15 +1,26 @@
 "use client";
 import { useMemo, useRef } from "react";
 import * as Scrollytelling from "@bsmnt/scrollytelling";
+import s from "./style.module.scss";
 
-export default function Ethos() {
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+export default function Ethos({ data }) {
+  console.log({ data });
+  const {
+    annotationsCollection,
+    copyBlock,
+    ethosCollageElement1,
+    ethosCollageElement2,
+    ethosCollageElemt3,
+    ethosImage,
+    illustration1,
+    illustration2,
+    largePullText,
+  } = data?.ethosSection;
   const containerRef = useRef(null);
 
-  const sectionData = [
-    { label: "SECTION 1" },
-    { label: "SECTION 2" },
-    { label: "SECTION 3" },
-  ];
+  const sectionData = [{ label: "SECTION 1" }, { label: "SECTION 2" }];
 
   const horizontalScrollTween = useMemo(() => {
     return { xPercent: -100 * (sectionData.length - 1), ease: "none" };
@@ -19,7 +30,7 @@ export default function Ethos() {
     <Scrollytelling.Animation
       key={index}
       tween={{
-        start: 0,
+        start: 50,
         end: 100,
         to: horizontalScrollTween,
       }}
@@ -38,6 +49,56 @@ export default function Ethos() {
     </Scrollytelling.Animation>
   ));
 
+  const Section1 = () => (
+    <Scrollytelling.Animation
+      tween={{
+        start: 50,
+        end: 100,
+        to: horizontalScrollTween,
+      }}
+    >
+      <div
+        style={{
+          width: "100vw",
+          height: "100%",
+        }}
+      >
+        <div>
+          DESTRUCTION
+          <br />
+          CREATION
+        </div>
+        <div className={s.introText}>
+          {documentToReactComponents(copyBlock.json)}
+        </div>
+      </div>
+    </Scrollytelling.Animation>
+  );
+
+  const Section2 = () => {
+    return (
+      <Scrollytelling.Animation
+        tween={{
+          start: 50,
+          end: 100,
+          to: horizontalScrollTween,
+        }}
+      >
+        <div
+          style={{
+            width: "100vw",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h2>section 2</h2>
+        </div>
+      </Scrollytelling.Animation>
+    );
+  };
+
   return (
     <Scrollytelling.Root defaults={{ ease: "none" }} scrub={1} end="+=3500">
       <section id="ethos">
@@ -48,7 +109,8 @@ export default function Ethos() {
           childClassName={"pin-style"}
           ref={containerRef}
         >
-          {sectionElements}
+          {Section1()}
+          {Section2()}
         </Scrollytelling.Pin>
       </section>
     </Scrollytelling.Root>
