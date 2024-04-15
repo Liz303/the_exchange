8c8a7fecf4aa39ff6954085621eeae46e82c7d3e
Mary E Henderson <lhenderson303@gmail.com>
2024-04-15 14:52:03 -0600

    test font

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "../styles/globals.scss";
 import Header from "@components/Header";
+import Script from "next/script";
 
 export const metadata = {
   title: "The Wolf's Tailor",
@@ -64,6 +65,28 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+<link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+<link rel="preload" href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet" as="style" />
+
+<style dangerouslySetInnerHTML={ {
+  __html: `</style>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet"
+      media="print"
+      onload="this.media='all';"
+    />
+    <style>`
+} }></style>
+
+<noscript>
+  <link
+    rel="stylesheet"
+    type="text/css"
+    href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet" />
+</noscript>
       <body className={` dark-theme`}>
         <Header />
         {children}
