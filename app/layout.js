a7b914197fc7832833aad6f7329fca1e2ea04291
IB - Scott <scott@impossible-bureau.com>
2024-04-24 08:51:53 -0600

    update favicon, sitemap and urls

--- app/layout.js
+++ app/layout.js
@@ -15,32 +15,32 @@ export const metadata = {
   icons: {
     icon: [
       { url: "/favicon-32x32.png" },
-      new URL("/favicon-32x32.png", "https://www.www.thewolfstailor.com/"),
+      new URL("/favicon-32x32.png", "https://www.thewolfstailor.com/"),
     ],
     icon: [
       { url: "/favicon-16x16.png" },
-      new URL("/favicon-16x16.png", "https://www.www.thewolfstailor.com/"),
+      new URL("/favicon-16x16.png", "https://www.thewolfstailor.com/"),
     ],
     apple: [{ url: "/apple-touch-icon.png" }],
     maskIcon: [{ url: "/safari-pinned-tab.svg", color: "#5bbad5" }],
   },
-  manifest: "https://www.www.thewolfstailor.com/site.webmanifest",
+  manifest: "https://www.thewolfstailor.com/site.webmanifest",
   twitter: {
     card: "summary_large_image",
     title: "The Wolf's Tailor",
     description:
       "A tribute to the artisans who crafted sheep’s attire for the cunning wolf. The innovative, multi-course tasting experience at the Wolf’s Tailor revolves around two prominent seasons: warmer months spotlight garden-fresh produce, while cooler months feature imaginative spins on local grains and fermentation. Accompanying our offerings are thoughtfully curated wine, mixed beverage, and zero-proof pairings, crafted from scratch ingredients and conscious spirits. Our ambitious culinary and beverage choices reflect a zero-waste ethos in both kitchen and bar, encompassing skillful techniques and compelling narratives inspired by our team’s journeys and influences.",
-    images: ["https://www.www.thewolfstailor.com/og-image.jpg"],
+    images: ["https://www.thewolfstailor.com/og-image.jpg"],
   },
   openGraph: {
     title: "The Wolf's Tailor",
     description:
       "A tribute to the artisans who crafted sheep’s attire for the cunning wolf. The innovative, multi-course tasting experience at the Wolf’s Tailor revolves around two prominent seasons: warmer months spotlight garden-fresh produce, while cooler months feature imaginative spins on local grains and fermentation. Accompanying our offerings are thoughtfully curated wine, mixed beverage, and zero-proof pairings, crafted from scratch ingredients and conscious spirits. Our ambitious culinary and beverage choices reflect a zero-waste ethos in both kitchen and bar, encompassing skillful techniques and compelling narratives inspired by our team’s journeys and influences.",
-    url: "https://www.www.thewolfstailor.com/",
+    url: "https://www.thewolfstailor.com/",
     siteName: "The Wolf's Tailor",
     images: [
       {
-        url: "https://www.www.thewolfstailor.com/og-image.jpg",
+        url: "https://www.thewolfstailor.com/og-image.jpg",
         width: 1257,
         height: 2400,
       },
@@ -48,7 +48,7 @@ export const metadata = {
     locale: "en_US",
     type: "website",
   },
-  metadataBase: new URL("https://www.www.thewolfstailor.com/"),
+  metadataBase: new URL("https://www.thewolfstailor.com/"),
   robots: {
     index: true,
     follow: true,
