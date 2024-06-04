ab4f9c8c1070fc5563d2179491c83b16165cea64
Mary E Henderson <lhenderson303@gmail.com>
2024-06-04 15:57:49 -0600

    intro files

--- containers/IntroSection/index.js
+++ containers/IntroSection/index.js
@@ -4,7 +4,9 @@ import gsap from "gsap";
 import * as Scrollytelling from "@bsmnt/scrollytelling";
 import s from "./style.module.scss";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-
+import BurgerAndBagel from "@/components/SVGs/burgerAndBagel";
+import Rosen from "@/components/SVGs/Rosen";
+import Burger from "@/components/SVGs/Burger";
 import { useWindowDimensions } from "@/hooks/useWindowDimensions";
 
 export default function Intro() {
@@ -21,10 +23,31 @@ export default function Intro() {
           invalidateOnRefresh: true,
         }}
       >
-        <div>
+        <div className={s.eyebrowContainer}>
             <h1 className={s.loaderHead}> WHEN DELI & DINNER Collide </h1>
         </div>
       </Scrollytelling.Root>
+        <div className={s.rowOne}>
+          <div className={s.columnOne}>
+           <div className={s.burgerAndBagel}>
+            <BurgerAndBagel />
+           </div>
+          </div>
+          <div className={s.columnTwo}>
+            <div className={s.quoteContainer}>
+              <span className="quote"> "Hey, I'm grillin' here!" </span>
+            </div>
+            <div className={s.highlight}>
+              Home of the $3.80 burger
+            </div>
+            <div className={s.rosen}>
+              <Rosen />
+            </div>
+          </div>
+        </div>
+        <div className={s.rowTwo}>
+          <Burger />
+        </div>
     </section>
   );
 }
