daed688518ce89e3b74781c55afbb4a9339d8c54
Mary E Henderson <lhenderson303@gmail.com>
2024-06-05 15:38:10 -0600

    animation and button and marquee

--- components/Marquee/index.js
+++ components/Marquee/index.js
@@ -1,9 +1,23 @@
+"use client";
 import Marquee from "react-fast-marquee";
 import styles from "./style.module.scss";
+import { gsap } from "gsap";
+import { useRef, useEffect } from "react";
 
 export default function FooteraMarquee() {
+    const marqueeRef = useRef();
+
+    useEffect(() => {
+        setTimeout(() => {
+            gsap.to(marqueeRef.current, {
+                y: 0,
+                duration: 1,
+            })
+          }, "2500");
+    }, [marqueeRef]);
+
     return (
-        <div className={styles.headerMarquee}>
+        <div className={styles.headerMarquee} ref={marqueeRef}>
             <Marquee>
             <h2>
                 HEY! I'M GRILLIN' HERE. HOME OF THE $3.80 BURGER &nbsp;
